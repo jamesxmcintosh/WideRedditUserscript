14beff864ea1d978eab742c89ff4bf25574949f5
jxm <jamesxmcintosh@users.noreply.github.com>
2020-06-10 23:10:32 +1000

    inital commit

---
+++ WideReddit.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Wide new reddit
+// @namespace   Violentmonkey Scripts
+// @match       https://www.reddit.com/*
+// @grant       none
+// @version     1.0
+// @license     GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
+// @icon        https://www.redditstatic.com/desktop2x/img/favicon/favicon-96x96.png
+// @author      jxm
+// @description Make new reddit layout fill screen width. 10/06/2020, 22:31:07
+// ==/UserScript==
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+addGlobalStyle('div { max-width: none !important; }');
