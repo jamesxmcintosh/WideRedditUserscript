f1f7ec627d20b8aa63312ac22497d14e0cdeffc4
jxm <jamesxmcintosh@users.noreply.github.com>
2021-01-06 12:02:38 +1100

    Fix for card view

--- WideReddit.user.js
+++ WideReddit.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name        Wide new reddit
+// @name        Wide new reddit | Userscript
 // @namespace   Violentmonkey Scripts
 // @match       https://www.reddit.com/*
 // @grant       none
-// @version     1.0
+// @version     1.2
 // @license     GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @icon        https://www.redditstatic.com/desktop2x/img/favicon/favicon-96x96.png
 // @author      jxm
-// @description Make new reddit layout fill screen width.
+// @description Make new reddit layout fill screen width. 10/06/2020, 22:31:07
 // ==/UserScript==
 function addGlobalStyle(css) {
     var head, style;
@@ -18,4 +18,5 @@ function addGlobalStyle(css) {
     style.innerHTML = css;
     head.appendChild(style);
 }
+addGlobalStyle('._1OVBBWLtHoSPfGCRaPzpTf._3nSp9cdBpqL13CqjdMr2L_ { width: 90% !important;}')
 addGlobalStyle('div { max-width: none !important; }');
